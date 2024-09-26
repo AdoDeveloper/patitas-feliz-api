2494eb2f018a52b1cf2d1c6a7aefc6d3b6f4eb9a
AdoDeveloper <ayudacuenta541@gmail.com>
2024-09-26 13:35:26 -0600

    Haciendo log de consola

--- src/index.js
+++ src/index.js
@@ -15,6 +15,12 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+// Middleware para registrar las peticiones
+app.use((req, res, next) => {
+    console.log(`${new Date().toISOString()} - ${req.method} ${req.originalUrl}`);
+    next(); // Pasar al siguiente middleware o ruta
+});
+
 // Utiliza las rutas con un prefijo
 app.use('/api', userRoutes);
 app.use('/api', appointmentRoutes);
@@ -30,6 +36,12 @@ prisma.$connect()
         console.error('Error al conectar con la base de datos:', error.message);
         process.exit(1);
     });
+
+// Middleware para manejar errores
+app.use((err, req, res, next) => {
+    console.error('Error:', err.message); // O err.stack para más detalles
+    res.status(500).json({ message: 'Ocurrió un error interno en el servidor' });
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
